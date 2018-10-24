33f17972f4ff64ee7c68768e467b6685a556b842
RichardReis <doktor9977@gmail.com>
2018-10-24 19:44:32 +0200

    Revert "Initial commit"
    This reverts commit 14725ab02f1c2c97a90cf50e265ff068a044fe29.

--- src/components/routes.js
+++
@@ -1,9 +0,0 @@
-import AddBlog from './AddBlog'
-import ShowBlogs from './showBlogs'
-import singleBlog from './singleBlog'
-
-export default[
-    {path:'/',component:ShowBlogs},
-    {path:'/add',component:AddBlog},
-    {path:'/blog/:id',component:singleBlog}
-]
